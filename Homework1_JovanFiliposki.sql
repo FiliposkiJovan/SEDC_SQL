8ccb3096d8b8a32d036ae22d7a9a5f19c82513f1
Jovan <filiposki.jovan@gmail.com>
2022-06-05 13:25:59 +0200

    Update Homework1_JovanFiliposki.sql
    changes

--- Homework1_JovanFiliposki.sql
+++ Homework1_JovanFiliposki.sql
@@ -2,70 +2,89 @@ CREATE DATABASE [SEDCACADEMY]
 
 USE[SEDCACADEMY]
 
-CREATE TABLE [Student]
+
+CREATE TABLE [dbo].[Student]
 (
-	Id INT IDENTITY(1,1) NOT NULL,
-	FirstName NVARCHAR(50) NOT NULL,
-	LastName NVARCHAR(50) NOT NULL,
-	DateOfBirth DATE NOT NULL,
-	EnrolledDate DATE NOT NULL,
-	Gander NVARCHAR(50) NOT NULL,
-	NationalIdNumber NVARCHAR(50) NOT NULL,
-	StudentCardNumber NVARCHAR(50) NOT NULL,
-	CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ( Id ASC )
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[DateOfBirth] [date] NOT NULL,
+	[EnrolledDate] [date] NOT NULL,
+	[Gender] [char](1) NOT NULL,
+	[NationalIDNumber] [nvarchar](50) NOT NULL,
+	[StudentCardNumber] [nvarchar](50) NOT NULL,
+	CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED 
+	(
+		[ID] ASC
+	)
 )
 
-CREATE TABLE [Teacher]
+CREATE TABLE [dbo].[Teacher]
 (
-	Id SMALLINT IDENTITY(1,1) NOT NULL,
-	FirstName NVARCHAR(50) NOT NULL,
-	LastName NVARCHAR(50) NOT NULL,
-	DateOfBirth DATE NOT NULL,
-	AcademicRank NVARCHAR(50) NOT NULL,
-	HireDate DATE NOT NULL,
-	CONSTRAINT [PK_Teacher] PRIMARY KEY CLUSTERED ( Id ASC )
+	[ID] [smallint] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[DateOfBirth] [date] NOT NULL,
+	[AcademicRank] [nvarchar](50) NOT NULL,
+	[HireDate] [date] NOT NULL,
+	CONSTRAINT [PK_Teacher] PRIMARY KEY CLUSTERED 
+	(
+		[ID] ASC
+	)
 )
 
-CREATE TABLE [Grade]
+CREATE TABLE [dbo].[Grade]
 (
-	Id INT IDENTITY(1,1) NOT NULL,
-	StudentId INT NOT NULL,
-	CourseId INT NOT NULL,
-	TeacherId INT NOT NULL,
-	Grade INT NOT NULL,
-	Comment NVARCHAR(250) NOT NULL,
-	CreatedDate DATETIME NOT NULL,
-	CONSTRAINT [PK_Grade] PRIMARY KEY CLUSTERED ( Id ASC )
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[StudentID] [int] NOT NULL,
+	[CourseID] [smallint] NOT NULL,
+	[TeacherID] [smallint] NOT NULL,
+	[Grade] [tinyint] NOT NULL,
+	[Comment] [nvarchar](100) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	CONSTRAINT [PK_Grade] PRIMARY KEY CLUSTERED 
+	(
+		[ID] ASC
+	)
 )
 
-CREATE TABLE [Course]
+CREATE TABLE [dbo].[Course]
 (
-	Id SMALLINT IDENTITY(1,1) NOT NULL,
-	[Name] NVARCHAR(50) NOT NULL,
-	Credit INT NOT NULL,
-	AcademicYear INT NOT NULL,
-	Semester INT NOT NULL,
-	CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED ( Id ASC )
+	[ID] [smallint] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Credit] [tinyint] NOT NULL,
+	[AcademicYear] [tinyint] NOT NULL,
+	[Semester] [tinyint] NOT NULL,
+	CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED 
+	(
+		[ID] ASC
+	)
 )
 
-CREATE TABLE [GradeDetails]
+CREATE TABLE [dbo].[GradeDetails]
 (
-	Id INT IDENTITY(1,1) NOT NULL,
-	GradeId INT NOT NULL,
-	AchivmentTypeId INT NOT NULL,
-	AchivmentPoints INT NOT NULL,
-	AchivmentMaxPoints INT NOT NULL,
-	AchivmentDate DATETIME NOT NULL,
-	CONSTRAINT [PK_GradeDetails] PRIMARY KEY CLUSTERED ( Id ASC )
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[GradeID] [int] NOT NULL,
+	[AchievementTypeID] [tinyint] NOT NULL,
+	[AchievementPoints] [tinyint] NOT NULL,
+	[AchievementMaxPoints] [tinyint] NOT NULL,
+	[AchievementDate] [datetime] NOT NULL,
+	CONSTRAINT [PK_GradeDetails] PRIMARY KEY CLUSTERED 
+	(
+		[ID] ASC
+	)
 )
 
-CREATE TABLE [AchivmentType]
-(
-	Id SMALLINT IDENTITY(1,1) NOT NULL,
-	[Name] NVARCHAR(50) NOT NULL,
-	[Description] NVARCHAR(50) NOT NULL,
-	ParticipationRate INT NOT NULL,
-	CONSTRAINT [PK_AchivmentType] PRIMARY KEY CLUSTERED ( Id ASC )
-)
 
+CREATE TABLE [dbo].[AchievementType]
+(
+	[ID] [tinyint] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](100) NULL,
+	[ParticipationRate] [tinyint] NOT NULL,
+	CONSTRAINT [PK_AchievementType] PRIMARY KEY CLUSTERED 
+	(
+		[ID] ASC
+	)
+)
 
